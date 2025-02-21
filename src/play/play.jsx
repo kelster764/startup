3ec3340df04732c691d9e500195c8aaaf1a97e5c
kelster764 <kpharmon@byu.edu>
2025-02-21 15:18:44 -0700

    messing around

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -2,44 +2,54 @@ import React from 'react';
 import './play_styles.css';
 
 export function Play() {
-  return (
-    <main>
-    <div className="players">
-      Player:
-      <span className="player-name">Mystery player</span>
-    </div>
-    <ul className="notification">
-      <li className="player-name">Tim started a new game</li>
-      <li className="player-name">Ada started a new game</li>
-      <li className="player-name">Tim scored 337</li>
-    </ul>
-
-
-    <br />
-
+  const [dead, setdead] = React.useState()
+  let dead = true
+  if(dead){
+    return(<div> dead dino </div>)
+  }
+  return(
     <div>
-      <label for="count">Score</label>
-      <input type="text" id="count" value="--" readonly />
-    </div>
-
-    <br />
-
-    <div>
-      <button>Reset</button>
-    </div>
-
-    <br />
-
-    <div className="game">
-      <div className="button-container">
-        <div className = "button-container2">
-        <button className="soprano"></button>
-        <button className="alto"></button>
-        <button className="tenor"></button>
-        <button className="bass"></button>
-        </div>
+      dino!
       </div>
-    </div>
-  </main>
-  );
+  )
+  // return (
+  //   <main>
+  //   <div className="players">
+  //     Player:
+  //     <span className="player-name">Mystery player</span>
+  //   </div>
+  //   <ul className="notification">
+  //     <li className="player-name">Tim started a new game</li>
+  //     <li className="player-name">Ada started a new game</li>
+  //     <li className="player-name">Tim scored 337</li>
+  //   </ul>
+
+
+  //   <br />
+
+  //   <div>
+  //     <label for="count">Score</label>
+  //     <input type="text" id="count" value="--" readonly />
+  //   </div>
+
+  //   <br />
+
+  //   <div>
+  //     <button>Reset</button>
+  //   </div>
+
+  //   <br />
+
+  //   <div className="game">
+  //     <div className="button-container">
+  //       <div className = "button-container2">
+  //       <button className="soprano"></button>
+  //       <button className="alto"></button>
+  //       <button className="tenor"></button>
+  //       <button className="bass"></button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </main>
+  // );
 }
