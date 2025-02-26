2ae852659296de426642b49cd175696057f9e49a
kelster764 <kpharmon@byu.edu>
2025-02-25 17:17:31 -0700

    react pt 2

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
+
 import './login.css';
 
 export function Login({ setUser }) {
-  setUser('Joe');
+  //setUser('Joe');
+
+  const [email, setEmail] = React.useState('');
+
+  const clicked = () => {
+    setUser(email);
+  
+  };
   return (
     <main>
       <section>
-        <form method='get' action='play.html'>
+
           <div>
             <span>@</span>
-            <input type='text' placeholder='your@email.com' />
+            <input type='text' placeholder='your@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            
           </div>
           <div>
             <span>🔒</span>
             <input type='password' placeholder='password' />
           </div>
-          <button type='submit'>Login</button>
+          <button type='submit' onClick={clicked}>Login</button>
           <button type='submit'>Create</button>
-        </form>
+    
       </section>
     </main>
   );
